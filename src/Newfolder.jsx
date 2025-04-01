7422c1f06675b9dbf9a6065e299e71ee2d9259c4
= <jainmuskaan1407@gmail.com>
2025-04-01 10:33:34 +0530

    this is my second file

---
+++ src/Newfolder.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Newfolder = () => {
+    return (
+        <>
+        <h1>I want to delete somthing
+            
+        </h1>
+        
+        </>
+    )
+}
